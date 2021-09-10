20c443145922da02fef091b3db69a9addb5fcdcd
Chiran Nuwan Bandara <37494456+nuwanchiran@users.noreply.github.com>
2021-09-10 12:50:08 +0530

    auto refresh the token when it expires to use the app continuesly

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -10,6 +10,10 @@ export default function useAuth(code) {
     getToken()
   }, [code])
 
+  useEffect(() => {
+    doRefreshToken()
+  },[expiresIn,refreshToken])
+
   const getToken = async () => {
 
     try {
@@ -24,8 +28,32 @@ export default function useAuth(code) {
       window.history.pushState({}, null, '/')
 
     } catch (e) {
-      console.error(e);
+      window.location = "/"
     }
+  }
+
+  const doRefreshToken = async () => {
+
+    if (!refreshToken || !expiresIn) return
+
+    const interval = setInterval(async () => {
+      try {
+        const res = await axios.post('http://localhost:9000/refresh', {refreshToken})
+
+        const {accessToken, expiresIn} = res.data;
+
+        setAccessToken(accessToken)
+        setExpiresIn(expiresIn)
+
+        // window.history.pushState({}, null, '/')
+
+      } catch (e) {
+        window.location = "/"
+      }
+    }, ((expiresIn - 60)* 1000));
+
+    return () => clearInterval(interval)
+
   }
 
   return accessToken
--- server/server.js
+++ server/server.js
@@ -6,6 +6,33 @@ const app = express()
 app.use(cors())
 app.use(express.json());
 
+
+app.post('/refresh',async (req,res) => {
+  const refreshToken = req.body.refreshToken
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "0c34f9d23dff4f7caea72df7c617b76a",
+    clientSecret: "56316e0d42e04f59927ea75effa2dcc8",
+    refreshToken,
+  })
+
+  try {
+    const data = await spotifyApi.refreshAccessToken()
+     // console.log(data.body);
+
+    const {access_token,expires_in} = data.body
+
+    res.status(200).json({
+      accessToken:access_token,
+      expiresIn:expires_in
+    })
+
+  } catch (e) {
+    res.sendStatus(400)
+  }
+
+})
 app.post('/login', async (req, res) => {
 
   const code = req.body.code
