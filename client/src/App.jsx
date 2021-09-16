2c46a87937400a6f51772d26d338e6efca19c2c9
Nisuga Jayawardana (IT18117110) <it18117110@my.sliit.lk>
2021-09-17 03:48:44 +0530

    wrapped with IonApp and IonReactRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,32 @@
+import { IonApp } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import {  Redirect, Route } from 'react-router-dom';
+import NoMatch from 'react-router-nomatch';
+
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
+import NotFound404 from './pages/404/404'
+
 import './App.css';
 
 const code = new URLSearchParams(window.location.search).get("code")
 
 function App() {
-  return (code? <Dashboard code={code}/>:<Login/>);
+
+  return(  
+    <IonApp>
+      <IonReactRouter>
+        <NoMatch component={NotFound404}>
+        <Route path="/" exact={true}>
+          {code? <Dashboard code={code}/>: <Redirect to="/login"/>}
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
+      </NoMatch>
+      </IonReactRouter>
+    </IonApp>
+  )
 }
 
 export default App;
