7e4bc848473f77d2909363578f98b9406d25def5
Nisuga Jayawardana (IT18117110) <it18117110@my.sliit.lk>
2021-09-17 04:53:10 +0530

    enhanced header and searchbar

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import useAuth from '../../hooks/useAuth'
 import { IonApp, IonButton, IonFooter, IonIcon, IonImg, IonSearchbar, IonThumbnail } from '@ionic/react';
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 
-
+import './../dashboard/dashboard.css'
 
 const Dashboard = ({code}) => {
   const accessToken = useAuth(code)
@@ -12,10 +12,18 @@ const Dashboard = ({code}) => {
     <IonPage>
       <IonHeader >
         <IonToolbar>
-          <IonButton slot="start" fill="clear">
-              <IonImg src="/assets/spotica-240.png" alt="Spotica Logo" style={{ height: "100%"}} />
+          <IonButton slot="start" fill="clear" onClick={()=> window.location = '/'}>
+              <IonImg src="/assets/spotica-240.png" alt="Spotica Logo" style={{ height: "100%"}}/>
             </IonButton>
-          <IonSearchbar placeholder="Search spotify"  slot="start"  fill="clear"/>
+          <IonSearchbar 
+            placeholder="Search spotify"  
+            slot="start"  
+            showCancelButton="never"
+            animated
+            debounce={400}
+            className="search-bar"
+            searchIcon={false}
+          />
         </IonToolbar>
       </IonHeader>
 
