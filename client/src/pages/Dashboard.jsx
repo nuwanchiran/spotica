51c34ea5c5577659afac2e699d867553a931044b
Nisuga Jayawardana (IT18117110) <it18117110@my.sliit.lk>
2021-09-17 02:42:11 +0530

    added ionic css and sample button

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import useAuth from '../hooks/useAuth'
+import { IonApp, IonButton } from '@ionic/react';
+
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './../theme/variables.css';
 
 const Dashboard = ({code}) => {
   const accessToken = useAuth(code)
+  
   return (
-    <div>
+    <IonApp>
+      <IonButton/>
       <h3>{code}</h3>
-    </div>
+    </IonApp>
   )
 }
 
