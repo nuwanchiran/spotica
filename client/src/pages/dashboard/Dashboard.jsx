d7726cd836b397ef564f04581471d6360b82174c
Nisuga Jayawardana (IT18117110) <it18117110@my.sliit.lk>
2021-09-17 13:33:37 +0530

    fetch tracks done. email allowed to API by chiran
    Co-authored-by: Chiran Nuwan Bandara <nuwanchiran@users.noreply.github.com>

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useAuth from '../../hooks/useAuth'
 import { IonApp, IonButton, IonFooter, IonIcon, IonImg, IonSearchbar, IonThumbnail } from '@ionic/react';
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import SpotifyWebApi from 'spotify-web-api-node';
 
 import './../dashboard/dashboard.css'
+
+const spotifyWebApi = new SpotifyWebApi({
+  clientId: process.env.REACT_APP_CLIENT_ID,
+})
 
 const Dashboard = ({code}) => {
   const accessToken = useAuth(code)
   
+  const [search, setSearch] = useState("")
+  const [foundData, setFoundData] = useState([])
+
+  useEffect(() => {
+    if(!accessToken) return;
+    spotifyWebApi.setAccessToken(accessToken)
+  },[accessToken])
+
+  useEffect(() => {
+    getTracks()
+  }, [search, accessToken])
+
+  const getTracks = async () => {
+    try{
+      if(!search) return setFoundData([])
+      if(!accessToken) return 
+      
+      let res = await spotifyWebApi.searchTracks(search)
+      console.log(res.body.tracks);
+    }catch (e){
+      console.error(e)
+    }
+  }
+
   return (
     <IonPage>
       <IonHeader >
@@ -23,6 +52,8 @@ const Dashboard = ({code}) => {
             debounce={400}
             className="search-bar"
             searchIcon={false}
+            value={search}
+            onIonChange={e=> { console.log(e.detail.value); setSearch(e.detail.value)}}
           />
         </IonToolbar>
       </IonHeader>
