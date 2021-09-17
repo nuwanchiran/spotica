1806d3980beca354181f7ea182f94a87b3951d0b
Nisuga Jayawardana (IT18117110) <it18117110@my.sliit.lk>
2021-09-18 00:28:47 +0530

    card bg basd on album image color variants

--- client/src/pages/dashboard/components/TrackCard.js
+++ client/src/pages/dashboard/components/TrackCard.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonIcon, IonImg, IonItem, IonLabel } from '@ionic/react'
 import Palette, { usePalette } from 'react-palette'
 import { play } from 'ionicons/icons';
 
-export default function TrackCard({ track }) {
+export default function TrackCard({ track, onClick }) {
 
     const { data, loading, error } = usePalette(track.albumUrl)
-    const gradient = `linear-gradient(${data.muted}, ${data.darkMuted})`
+    const ionItemRef = useRef(null)
+
+    useEffect(() => {
+       const gradient = `linear-gradient(${data.vibrant}, ${data.darkVibrant})`
+       ionItemRef.current.style.setProperty('--background', gradient);
+       //ionItemRef.current.style.setProperty('--background', `radial-gradient( circle farthest-corner at 10% 20%,  ${data.lightVibrant} 0%, ${data.light} 90% )`);
+    }, [data])
 
     return (
-        <IonCard style={{ background: 'red', width: "100%"}}>
-            <IonItem >
+        <IonCard button style={{ width: "100%"}} onClick={onClick}>
+            <IonItem ref={ionItemRef}>
                 <IonImg src={track.albumUrl} alt={track.albumUrl} slot="start" />
-                <IonLabel > 
-                    <IonCardTitle>{track.name}</IonCardTitle>
-                    <IonCardSubtitle>{track.artist}</IonCardSubtitle>
+                <IonLabel> 
+                    <IonCardTitle style={{color: "white", fontWeight: "bold"}}>{track.name}</IonCardTitle>
+                    <IonCardSubtitle style={{color: "white"}}>{track.artist}</IonCardSubtitle>
                 </IonLabel>
                 <IonButton  shape="round" fill="clear" color="medium" size="large" slot="end" >
                     <IonIcon icon={play}/>
